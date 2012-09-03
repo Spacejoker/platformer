44fe8d56b281170b01c61cf41d404d8a379a285d
jensa <jensa@jensa-VirtualBox.(none)>
2012-09-03 18:28:20 +0200

    working animation and moving right/left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import pygame, sys, os
-from Model import *
-
 
 class Player_action:
 	WALK_LEFT=0
@@ -16,6 +14,10 @@ class Player:
 		self.animations[0] = Animation(pygame.image.load("sprite1.png"), (64,64), 2, 15)
 		self.animations[1] = Animation(pygame.image.load("sprite2.png"), (64,64), 2, 15)
 		self.direction = Direction.LEFT
+		self.rundir = None
+		self.pos = (0,500)
+		self.acceleration = 0
+		self.speed = 0
 
 	def get_image(self):
 		if self.direction == Direction.LEFT:
@@ -31,7 +33,6 @@ class Animation:
 		self.cnt = 0
 		self.cur_frame = 0
 		self.num_images = num_images
-
 
 	def tick(self):
 		self.cnt += 1
@@ -63,13 +64,21 @@ def main():
 	
 		events = pygame.event.get()
 		for e in events:
+			print e
 			if e.type == pygame.KEYDOWN:
 				if e.key == pygame.K_SPACE:
 					active = False
-				if e.key == pygame.K_LEFT:
-					p.direction = Direction.LEFT
-				if e.key == pygame.K_RIGHT:
-					p.direction = Direction.RIGHT
+
+		
+		left_pressed = pygame.key.get_pressed()[pygame.K_LEFT]
+		right_pressed =	pygame.key.get_pressed()[pygame.K_RIGHT]
+		if left_pressed != right_pressed:
+			if left_pressed:
+				p.rundir = Direction.LEFT
+			else:
+				p.rundir = Direction.RIGHT
+		else:
+			p.rundir = None
 
 		T = pygame.time.get_ticks()
    
@@ -79,20 +88,39 @@ def main():
 		while(T-now >= step_size):
 			now += step_size
 			#logics
+			logic(p)
 		else:
 			pygame.time.wait(10)
 	
 		#render
-		BG_COLOR = 150, 150, 50
+		BG_COLOR = 50, 50, 50
 		screen.fill(BG_COLOR)
-	#	screen = pygame.display.get_surface()
-	#	fst = img.subsurface(pygame.Rect (0,0,64,64))
-	#	snd = img.subsurface(pygame.Rect (64,0,64,64))
-		
-		screen.blit(p.get_image(), (0,0))
+		screen.blit(p.get_image(), p.pos)
 
 		pygame.display.flip()
-	 
+	
+def logic(p):
+	if p.rundir == None:
+		p.acceleration = 0
+		p.speed /= 2
+		if(p.speed < 0.2):
+			p.speed = 0
+	elif p.rundir == Direction.LEFT:
+		p.direction = Direction.LEFT
+		p.acceleration = -5
+		if p.speed > 0:
+			p.speed /= 2
+	elif p.rundir == Direction.RIGHT:
+		p.direction = Direction.RIGHT
+		p.acceleration = 5
+		if p.speed < 0:
+			p.speed /= 2
+
+	p.speed += 0.1 * p.acceleration
+	p.speed = max(-4.0, p.speed)
+	p.speed = min(4.0, p.speed) 
+	p.pos = (p.pos[0] + p.speed, p.pos[1])
+
 if __name__ == "__main__":
 	main()
 
